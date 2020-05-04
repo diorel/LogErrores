baea17426ad36f4303e4df4562680e7592f510ff
Diorelyon Amador <diorel_x@hotmail.com>
2020-05-04 11:48:42 -0500

    repo logs
    funcion que permite guardar logs en un txt

---
+++ LogErrores/LogErrores/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogErrores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Log oLog = new Log(@"C:\Users\diore\source\repos\LogErrores\LogErrores\");
+            Log oLog = new Log(@"..\\..\\Logs");
+            oLog.Add("Hola prueba ruta dinamica");
+        }
+    }
+}
